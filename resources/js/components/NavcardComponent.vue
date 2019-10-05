19c84c227739bf16b4b1bf1bb3fdd3af1710ab5b
pedobearbn21 <pedobearearn@gmail.com>
2019-10-05 22:37:14 +0700

    [Edited] Navbar css

---
+++ resources/js/components/NavcardComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <b-card class="navcard" title="Title" header-tag="header" footer-tag="footer">
+        <template v-slot:header>
+            <h6 class="mb-0">Header Slot</h6>
+        </template>
+        <b-card-text>Header and footers using slots.</b-card-text>
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+        <template v-slot:footer>
+            <em>Footer Slot</em>
+        </template>
+    </b-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,8 +29,9 @@
                 <!-- <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel')    }}
                 </a> -->
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+                <button class=" openbtn" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <!-- <span class="navbar-toggler-icon"></span> -->
+                    &#9776;
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
