69d067e0fc9fd471df03e316ef4f9b4261156eb1
Jirayu321 <50855666+Jirayu321@users.noreply.github.com>
2019-10-10 03:18:37 +0700

    [added]

--- resources/js/components/Lesson1Component.vue
+++ resources/js/components/Lesson1Component.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="container table-responsive" style="text-align: -webkit-center;">
-
         <b-card  text-variant="white"  style="width: 150px;      border-radius: 10px;" class="text-center my-2 bodycard">
             <b-card-text>บทที่1</b-card-text>
         </b-card>
@@ -96,6 +95,248 @@
             </tr>
         </tbody>
         </table>
+        <p></p>
+    <div>
+      <b-card class="bodycard text-left">
+          Exercise
+          <p>Fill in the blanks with the most appropriate words.</p>
+          <p>1.  The __________ of booking tickets in advance is that you get better seats.</p>
+          <p>2.  My thoughts were __________ by the sound of the doorbell.</p>
+          <p>3.  ncreasing the number of women in top management jobs will be a slow __________.</p>
+          <p>4.  Police questioned the prisoner for several hours, but were unable to __________ any further information.</p>
+          <p>5.  You can use the device to scan an image and reproduce it on-screen in an __________ format.</p>
+          <p>6.  The second section is the Review of Literature, and is __________ of three major subheadings. </p>
+          <p>7.  He was a __________ of the Civilian Service Award</p>
+          <p>8.  Profitability is the major aim of banks and most other financial __________.</p>
+          <p>9.  The exhibition has received a positive __________ from visitors. </p>
+          <p>10. The information we received is of a highly __________ nature and relates to national security.</p>
+      </b-card>
+    </div>
+     <div>
+      <b-card class="bodycard text-left mt-3">          
+           LanguageFocus
+          <p class="text-center">The correct use of salutations and closing expressions</p>
+          &emsp;The correct salutation may depend on a company’s e-mail policy. The table below gives an overview of the possible salutations that can be used. In addition, the table includes the closing or ending expressions that can be used in an e-mail, which should correspond to the salutation.Informal salutation means informal closing statement; formal salutation means formal closing statement; no salutation means no closing statement.
+        <table class="table bodycard table-borderless text-center mt-3">
+            <thead>
+                <tr class="text-left">
+                    <th scope="col">Type</th>
+                    <th scope="col">Salutations</th>
+                    <th scope="col">Closings</th>
+                </tr>
+                    </thead>
+                    <tbody>
+                <tr class="text-left">            
+                    <td>1. You do not know who you <br/>are writing to:</td>
+                    <td>– Dear Sir or Madam<br/>
+                    -Dear clients</td>
+                    <td>- Yours faithfully</td>
+                </tr>
+                <tr class="text-left">            
+                    <td>2. You know the person but <br/>you’ve never written to or<br/> met the person:</td>
+                    <td>– Dear Mr. Smith<br/>– Dear Mrs. Jones<br/>– Dear Dr. Young</td>
+                    <td>– Yours sincerely<br/>– Sincerely (yours)<br/>– Cordially yours</td>
+                </tr>
+                <tr class="text-left">            
+                    <td>3. The person is a little bit closer: </td>
+                    <td>- Dear Sophie Jones </td>
+                    <td>– Best regards<br/>– With best regards</td>
+                
+                </tr>
+                <tr class="text-left">            
+                    <td>4. The person is a close business contact or <br/>she/he has signed her or his e-mail with the <br/>first name:</td>
+                    <td>– Dear Sophie<br/>– Hello, Sophie<br/>– Hi, Sophie<br/>– Sophie<br/>– Hi<br/>– Hello</td>
+                    <td>– Best regards<br/>– With best regards<br/>– Kind regards<br/>– Best wishes</td>
+                </tr>
+                          
+            </tbody>
+        </table>
+    </b-card>
+    </div>
+    <div>
+      <b-card class="bodycard text-left mt-3">          
+           
+          <p class="text-center">Opening sentences</p>
+          &emsp;Use one of the following phrases to refer to an earlier message sent or received, or to give a reason for wishing to contact someone.
+
+        <table class="table bodycard table-borderless text-center mt-3">
+            <thead>
+                <tr class="text-left">
+                    <th scope="col">Formal: Referring to an earlier message sent or received</th>
+                   
+                </tr>
+                    </thead>
+                    <tbody>
+                <tr class="text-left">            
+                    <td>- I am writing with regard to your recent e-mail.<br/>- Referring to your request for information, …<br/>- I’m writing with reference to order number KULIP1.<br/>
+                    - Further to your last e-mail, …<br/>- I am mailing this via the ‘Contact us’ link on your web shop. I would like to ask you …</td>
+                   
+                </tr>
+           
+          
+          
+     
+            </tbody>
+        </table>
+        <table class="table bodycard table-borderless text-center mt-3">
+            <thead>
+                <tr class="text-left">
+                    <th scope="col">Informal: Referring to an earlier message sent or received</th>
+                   
+                </tr>
+                    </thead>
+                    <tbody>
+                <tr class="text-left">            
+                    <td>- Just a quick note to say I really appreciated …<br/>- I got your name from Dr. Smith.<br/>- Re your e-mail … (instead of formal: Further to your last e-mail …)</td>
+                   
+                </tr>
+           
+          
+          
+     
+            </tbody>
+        </table>
+        <table class="table bodycard table-borderless text-center mt-3">
+            <thead>
+                <tr class="text-left">
+                    <th scope="col">Giving a reason for wishing to contact someone</th>
+                   
+                </tr>
+                    </thead>
+                    <tbody>
+                <tr class="text-left">            
+                    <td>- Our reason for contacting you is the following: …<br/>- As discussed this morning in our telephone conversation, …<br/>- It is our pleasure to inform you of …<br/>- As we agreed during …<br/>- As requested in your e-mail of …<br/>- I am writing in connection with …<br/>- We would like to inform you about …<br/>- We would like to draw your attention to the following: …
+<br/>- Thank you for your e-mail and your interest in ...<br/>- Thank you for the enquiry you made via our website</td>
+                   
+                </tr>
+           
+          
+          
+     
+            </tbody>
+        </table>
+        <table class="table bodycard table-borderless text-center mt-3">
+            <thead>
+                <tr class="text-left">
+                    <th scope="col">Indicate attachments</th>
+                   
+                </tr>
+                    </thead>
+                    <tbody>
+                <tr class="text-left">            
+                    <td>- Enclosed please find the necessary technical specifications<br/>- We are happy to enclose …<br/>- A route description has been enclosed. <br/>- For the general terms please refer to the attachment. <br/>- Please see our prices on enclosed price list.<br/>- Enclosed please find our latest catalogue. <br/>- Please find enclosed some low resolution jpg images. <br/>- Please find attached my report.<br/>- I’m sending you our general conditions as a PDF file.<br/>- I have attached the revised quarterly budget. Could you forward it to all the Düsseldorf
+managers?</td>
+                   
+                </tr>
+           
+          
+          
+     
+            </tbody>
+        </table>
+    </b-card>
+    </div>
+    <div>
+      <b-card class="bodycard text-left mt-3">          
+           
+          <p class="text-center">Ending an E-mail</p>
+          &emsp;t is common to include a closing remark in an e-mail to let readers know that they have reached the end of a message. A closing remark may also be used to express your gratitude, or what you expect the reader to do (e.g. answer, provide information, etc.).
+
+      <table class="table bodycard table-borderless text-center mt-3">
+            <thead>
+                <tr class="text-left">
+                    <th scope="col">Formal: Closing remarks</th>
+                   
+                </tr>
+                    </thead>
+                    <tbody>
+                <tr class="text-left">            
+                    <td>- I look forward to hearing from you soon.<br/>- I look forward to receiving your advice on this matter.<br/>- We should be glad to receive this information<br/>- We hope we have been of help to you.<br/>- We trust we have furnished you with all the necessary information.
+<br/>- We hope for an early reply.<br/>- I look forward to receiving the information as soon as possible.</td>
+                   
+                </tr>
+           
+          
+          
+     
+            </tbody>
+        </table>
+        <table class="table bodycard table-borderless text-center mt-3">
+            <thead>
+                <tr class="text-left">
+                    <th scope="col">Informal: Closing remarks</th>
+                   
+                </tr>
+                    </thead>
+                    <tbody>
+                <tr class="text-left">            
+                    <td>- I’m looking forward to … (+ ~ing).<br/>- Please feel free to contact me.<br/>- If you’d like more details, let me know<br/>- Just give me a call if you have any questions<br/>- Have a nice weekend.<br/>- Speak to you soon. <br/>- I would appreciate a reply asap.
+<br/>- Please deal with this matter urgently. <br/>- Can I expect a reply from you by tomorrow morning, please?</td>
+                   
+                </tr>
+           
+          
+          
+     
+            </tbody>
+        </table>
+        <table class="table bodycard table-borderless text-center mt-3">
+            <thead>
+                <tr class="text-left">
+                    <th scope="col">Offering further information or services</th>
+                   
+                </tr>
+                    </thead>
+                    <tbody>
+                <tr class="text-left">            
+                    <td>- Should you need any further information about … we will be happy to assist you.<br/>- If you’d like any more details, just let us know.<br/>- Should you have any further questions, we are at your disposal. <br/>- If we can be of service in any way?</td>
+                   
+                </tr>
+           
+          
+          
+     
+            </tbody>
+        </table>
+        <table class="table bodycard table-borderless text-center mt-3">
+            <thead>
+                <tr class="text-left">
+                    <th scope="col">Thanks</th>
+                   
+                </tr>
+                    </thead>
+                    <tbody>
+                <tr class="text-left">            
+                    <td>- Finally, we wish to express our appreciation for the cooperation we received from your company’s employees during the audit.<br/>- Thank you again for your interest in our company.
+<br/>- Thank you in advance for your cooperation.</td>
+                   
+                </tr>
+           
+          
+          
+     
+            </tbody>
+        </table>
+        <table class="table bodycard table-borderless text-center mt-3">
+            <thead>
+                <tr class="text-left">
+                    <th scope="col">Announcing activities</th>
+                   
+                </tr>
+                    </thead>
+                    <tbody>
+                <tr class="text-left">            
+                    <td>- I hope I may contact you later on this matter<br/>- Mr./Mrs. … will contact you at an early date to explain the details.<br/>- We’ll inform you on a weekly basis about …<br/>- We will forward the report as soon as possible.<br/>- We’ll be glad to provide you with further details.<br/>- We shall inform you as soon as we have the requested products in stock again.</td>
+                   
+                </tr>
+           
+          
+          
+     
+            </tbody>
+        </table>
+    </b-card>
+    </div>
     </div>
 </template>
 
