83a03e244efaa609a34c3e0f178fa00c2756b648
france4543 <50855675+france4543@users.noreply.github.com>
2019-10-09 22:46:55 +0700

    [Edied] Vocab

--- resources/js/components/Lesson5Component.vue
+++ resources/js/components/Lesson5Component.vue
@@ -1,89 +1,112 @@
 <template>
-   <div class="container table-responsive" style="text-align: -webkit-center;">
+  <div class="container table-responsive" style="text-align: -webkit-center; color:white;">
+    <b-card
+      text-variant="white"
+      style="width: 150px;      border-radius: 10px;"
+      class="text-center my-2 bodycard"
+    >
+      <b-card-text>บทที่5</b-card-text>
+    </b-card>
+    <table class="table bodycard table-borderless text-center">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Word</th>
+          <th scope="col">Part of Speech</th>
+          <th scope="col">Mean</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>academic</td>
+          <td>adj.</td>
+          <td
+            class="text-left"
+          >Relating to schools, colleges ,and universities or connected with studying and thinking</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>conference</td>
+          <td>n.</td>
+          <td class="text-left">
+            An event, at which there is a group of talks on a particular subject, or a meeting in which
+            <p>particular matters are discussed formally</p>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>bullet point(s)</td>
+          <td>n.</td>
+          <td class="text-left">A small symbol used to introduce each item in a list, for emphasis.</td>
+        </tr>
+        <tr>
+          <th scope="row">4</th>
+          <td>consist</td>
+          <td>v.</td>
+          <td class="text-left">To be composed or made up of.</td>
+        </tr>
+        <tr>
+          <th scope="row">5</th>
+          <td>integrate</td>
+          <td>v.</td>
+          <td class="text-left">Combine things with another to from a whole.</td>
+        </tr>
+        <tr>
+          <th scope="row">6</th>
+          <td>dynamic</td>
+          <td>adj.</td>
+          <td class="text-left">(of a person) Positive in attitute and full of energy and new ideas.</td>
+        </tr>
+        <tr>
+          <th scope="row">7</th>
+          <td>require</td>
+          <td>v.</td>
+          <td class="text-left">To show needs for a particular purpose.</td>
+        </tr>
+        <tr>
+          <th scope="row">8</th>
+          <td>offensive</td>
+          <td>adj.</td>
+          <td class="text-left">Causing someone to feel rasentful, upset, or annoyed.</td>
+        </tr>
+        <tr>
+          <th scope="row">9</th>
+          <td>relevant</td>
+          <td>adj.</td>
+          <td
+            class="text-left"
+          >Closely connected or appropriate to what is being done or considered.</td>
+        </tr>
+        <tr>
+          <th scope="row">10</th>
+          <td>achieve</td>
+          <td>v.</td>
+          <td
+            class="text-left"
+          >Successfully bring about or reach (goals) through effort, skill, or courage.</td>
+        </tr>
+      </tbody>
+    </table>
 
-        <b-card  text-variant="white"  style="width: 150px;      border-radius: 10px;" class="text-center my-2 bodycard">
-            <b-card-text>บทที่5</b-card-text>
-        </b-card>
-        <table class="table bodycard table-borderless text-center">
-        <thead>
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">Word</th>
-            <th scope="col">Part of Speech</th>
-            <th scope="col">Mean</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <th scope="row">1</th>
-            <td>academic</td>
-            <td>adj.</td>
-            <td class="text-left">Relating to schools, colleges ,and universities or connected with studying and thinking</td>
-            </tr>
-            <tr>
-            <th scope="row">2</th>
-            <td>conference</td>
-            <td>n.</td>
-            <td class="text-left">An event, at which there is a group of talks on a particular subject, or a meeting in which<p>particular matters are discussed formally</p></td>
-            </tr>
-            <tr>
-            <th scope="row">3</th>
-            <td>bullet point(s)</td>
-            <td>n.</td>
-            <td class="text-left">A small symbol used to introduce each item in a list, for emphasis.</td>
-            </tr>
-            <tr>
-            <th scope="row">4</th>
-            <td>consist</td>
-            <td>v.</td>
-            <td class="text-left">To be composed or made up of.</td>
-            </tr>
-            <tr>
-            <th scope="row">5</th>
-            <td>integrate</td>
-            <td>v.</td>
-            <td class="text-left">Combine things with another to from a whole.</td>
-            </tr>
-            <tr>
-            <th scope="row">6</th>
-            <td>dynamic</td>
-            <td>adj.</td>
-            <td class="text-left">(of a person) Positive in attitute and full of energy and new ideas.</td>
-            </tr>
-            <tr>
-            <th scope="row">7</th>
-            <td>require</td>
-            <td>v.</td>
-            <td class="text-left">To show needs for a particular purpose.</td>
-            </tr>
-            <tr>
-            <th scope="row">8</th>
-            <td>offensive</td>
-            <td>adj.</td>
-            <td class="text-left">Causing someone to feel rasentful, upset, or annoyed.</td>
-            </tr>
-            <tr>
-            <th scope="row">9</th>
-            <td>relevant</td>
-            <td>adj.</td>
-            <td class="text-left">Closely connected or appropriate to what is being done or considered.</td>
-            </tr>
-            <tr>
-            <th scope="row">10</th>
-            <td>achieve</td>
-            <td>v.</td>
-            <td class="text-left">Successfully bring about or reach (goals) through effort, skill, or courage.</td>
-            </tr>
-            
-        </tbody>
-        </table>
+    <div>
+      <b-card no-body class="text-center">          
+        <div class="bg-secondary text-light">
+            <div class="text-left">
+                Exercise
+                <p>Fill in the blanks with the most appropriate words.</p>
+                <p>1. The idea with young children is to _______ learning with play.</p>
+                <p>2. The participants in this study _______ of 27 males and 31 females.</p>
+                <p>3. Many education studens decide to do their internship training at famous _______ institutions.</p>
+            </div> 
+          
+        </div>
+      </b-card>
     </div>
-
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
