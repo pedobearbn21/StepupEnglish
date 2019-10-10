3c17d64ba240112270b4aba95d8f63213f9f9caa
kathawut <tamza1094@hotmail.com>
2019-10-10 16:22:14 +0700

    [edited] lesson3 chaged font

--- resources/js/components/Lesson3Component.vue
+++ resources/js/components/Lesson3Component.vue
@@ -1,10 +1,9 @@
 <template>
     <div class="container table-responsive" style="text-align: -webkit-center;">
-
         <b-card  text-variant="white"  style="width: 150px;      border-radius: 10px;" class="text-center my-2 bodycard">
             <b-card-text>บทที่3</b-card-text>
         </b-card>
-        <table class="table bodycard table-borderless text-center table-hover">
+        <table class="table bodycard table-borderless text-center   table-hover">
         <thead>
             <tr>
             <th scope="col">#</th>
@@ -16,153 +15,118 @@
         <tbody>
             <tr>
             <th scope="row">1</th>
-            <td>tabloid (or  tabloid newspaper) </td>
+            <td>tabloid <br/>(or  tabloid newspaper) </td>
             <td>n.</td>
-            <td>A gossip newspaper that has small pages, short articles, and lots of photographs. </td>
+            <td class="text-left">A gossip newspaper that has small pages, short articles, and lots of photographs. </td>
             </tr>
             <tr>
             <th scope="row">2</th>
             <td>quarterly</td>
-            <td>adj</td>
-            <td>Produced or happening every three months.</td>
+            <td>adj.</td>
+            <td class="text-left">Produced or happening every three months.</td>
             </tr>
             <tr>
             <th scope="row">3</th>
             <td>stay tuned</td>
-            <td>v (idiom)</td>
-            <td>Continue to pay attention to this matter.  Watch for further developments.</td>
+            <td>v. (idiom)</td>
+            <td class="text-left">Continue to pay attention to this matter.  Watch for further developments.</td>
             </tr>
             <tr>
             <th scope="row">4</th>
             <td>abroad</td>
-            <td>adv</td>
-            <td>In or going to a foreign country</td>
+            <td>adv.</td>
+            <td class="text-left">In or going to a foreign country</td>
             </tr>
             <tr>
             <th scope="row">5</th>
             <td>opinion</td>
-            <td>n</td>
-            <td>A belief, judgment, or way of thinking about something.</td>
+            <td>n.</td>
+            <td class="text-left">A belief, judgment, or way of thinking about something.</td>
             </tr>
             <tr>
             <th scope="row">6</th>
             <td>issue</td>
-            <td>n</td>
-            <td>A subject that people discuss or argue about, especially relating to society, politics, etc.</td>
+            <td>n.</td>
+            <td class="text-left">A subject that people discuss or argue about, especially relating to society, politics, etc.</td>
             </tr>
             <tr>
             <th scope="row">7</th>
             <td>event</td>
-            <td>n</td>
-            <td>An organized occasion such as a party or sports competition.</td>
+            <td>n.</td>
+            <td class="text-left">An organized occasion such as a party or sports competition.</td>
             </tr>
             <th scope="row">8</th>
             <td>notice</td>
-            <td>n</td>
-            <td>A small advertisement or announcement in a newspaper or magazine.</td>
+            <td>n.</td>
+            <td class="text-left">A small advertisement or announcement in a newspaper or magazine.</td>
             </tr>
             <tr>
             <th scope="row">9</th>
             <td>commodity</td>
-            <td>n</td>
-            <td>Something that can be bought and sold, especially basic food products or fuel.</td>
+            <td>n.</td>
+            <td class="text-left">Something that can be bought and sold, especially basic food products or fuel.</td>
             </tr>
              <tr>
             <th scope="row">10</th>
             <td>horoscope</td>
-            <td>n</td>
-            <td>A description of what is going to happen to sb in the future, based on the position of the stars and the planets when the person was born.</td>
+            <td>n.</td>
+            <td class="text-left">A description of what is going to happen to sb in the future,<br/> based on the position of the stars and the planets when the person was born.</td>
             </tr>
              <tr>
             <th scope="row">11</th>
             <td>budget</td>
-            <td>v</td>
-            <td>To plan to spend (an amount of money) for a particular purpose.</td>
+            <td>v.</td>
+            <td class="text-left">To plan to spend (an amount of money) for a particular purpose.</td>
             </tr>
              <tr>
             <th scope="row">12</th>
             <td>occasion</td>
-            <td>n</td>
-            <td>A particular time when sth happens.</td>
+            <td>n.</td>
+            <td class="text-left">A particular time when sth happens.</td>
             </tr>
              <tr>
             <th scope="row">13</th>
             <td>schedule</td>
-            <td>n</td>
-            <td>A list that shows when buses, trains, etc. will arrive and leave. </td>
+            <td>n.</td>
+            <td class="text-left">A list that shows when buses, trains, etc. will arrive and leave. </td>
             </tr>
              <tr>
             <th scope="row">14</th>
             <td>obituary</td>
-            <td>n</td>
-            <td>A report in a newspaper that announces someone's death and gives a short description of their life and achievements.</td>
+            <td>n.</td>
+            <td class="text-left">A report in a newspaper that announces someone's death and gives<br/> a short description of their life and achievements.</td>
             </tr>
              <tr>
             <th scope="row">15</th>
             <td>fellowship</td>
-            <td>n</td>
-            <td>A friendly relationship among people, especially of people who share the same interests or feelings.</td>
+            <td>n.</td>
+            <td class="text-left">A friendly relationship among people, especially of people who share <br/>the same interests or feelings.</td>
             </tr>
         </tbody>
         </table>
-        <br></br>
-        <table class="table bodycard table-borderless text-left table-hover">
-        <thead>
-            <tr>
-            <th scope="col"><h3>Vocabulary Exercise</h3></th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1. Details of meetings and social _______________ are sent out to all club members.</th> 
-            </tr>
-            <tr>
-                <th scope="row">2. The music band will have more music coming up, so ________   _and you won’t miss the newest singles.</th> 
-            </tr>
-            <tr>
-                <th scope="row">3. Personal ______________ in newspapers cover events such as  births, deaths, funerals, marriages, engagements, anniversaries, and birthdays.</th> 
-            </tr>
-            <tr>
-                <th scope="row">4. We usually ______________ for a family holiday that will cost about two thousand pounds.</th> 
-            </tr>
-            <tr>
-                <th scope="row">5. These workshops provide a (an) ______________ for talking about art.</th> 
-            </tr>
-            <tr>
-                <th scope="row">6. ______________ among the parishioners at the local church was evident with their weekly prayer gatherings and social events.</th> 
-            </tr>
-            <tr>
-                <th scope="row">7. The ______________ are full of stories about Jennifer Lawrence and her boyfriend.</th> 
-            </tr>
-            <tr>
-                <th scope="row">8. I would love to go ______________ this year, perhaps to the South of France.</th> 
-            </tr>
-            <tr>
-                <th scope="row">9. The students were not given the chance to express a (an) ______________.</th> 
-            </tr>
-            <tr>
-                <th scope="row">10. It's a website devoted to environmental ______________. </th> 
-            </tr>
-            <tr>
-                <th scope="row">11. My _____________ said I was going to be lucky in love this month.</th> 
-            </tr>
-            <tr>
-                <th scope="row">12. Writing a (an) ______________ is a way to honor your loved one's life as well as to announce their death. It can be a painful process.</th> 
-            </tr>
-            <tr>
-                <th scope="row">13. Since January, the new issue of this ______________ magazine will be published again in April.  </th> 
-            </tr>
-            <tr>
-                <th scope="row">14. The jet arrived in Johannesburg two minutes ahead of ______________. </th> 
-            </tr>
-            <tr>
-                <th scope="row">15. The government increased prices on several basic ______________ like bread and meat.</th> 
-            </tr>
-        </tbody>
-        </table>
-        <br></br>
-         <table class="table bodycard table-borderless text-center table-hover">
-         <thead><h3 class = "center">Present Perfect Tense</h3></thead>
+            <p></p>
+    <div>
+      <b-card class="bodycard text-left">
+          <p class="font-big">Vocabulary Exercise</p>
+          <p>1.  Details of meetings and social _______________ are sent out to all club members.</p>
+          <p>2.  The music band will have more music coming up, so ________   _and you won’t miss the newest singles.</p>
+          <p>3.  Personal _________in newspapers cover events such as  births, deaths, funerals, marriages, engagements, anniversaries, and birthdays.</p>
+          <p>4.  We usually __________ for a family holiday that will cost about two thousand pounds.</p>
+          <p>5.  These workshops provide a (an) ______________ for talking about art.</p>
+          <p>6.  ______________ among the parishioners at the local church was evident with their weekly prayer gatherings and social events.</p>
+          <p>7.  The ______________ are full of stories about Jennifer Lawrence and her boyfriend.</p>
+          <p>8.  I would love to go ______________ this year, perhaps to the South of France.</p>
+          <p>9.  The students were not given the chance to express a (an) ______________.</p>
+          <p>10. It's a website devoted to environmental ______________.</p>
+          <p>11. My _____________ said I was going to be lucky in love this month.</p>
+          <p>12. Writing a (an) ______________ is a way to honor your loved one's life as well as to announce their death. It can be a painful process.</p>
+          <p>13. Since January, the new issue of this ______________ magazine will be published again in April.</p>
+          <p>14. The jet arrived in Johannesburg two minutes ahead of ______________.</p>
+          <p>15. The government increased prices on several basic ______________ like bread and meat.</p>
+      </b-card>
+    </div>
+         <table class="table bodycard table-borderless text-center mt-3">
+        
         <thead>
             <tr>
             <th scope="col"></th>
