6c06e3b25bee87846337a59135332edf1f08fe92
Pakorn Tawansang <pagorn56110@gmail.com>
2019-10-10 17:16:17 +0700

    [Edited] Lesson2 color form

--- resources/js/components/Lesson2Component.vue
+++ resources/js/components/Lesson2Component.vue
@@ -5,7 +5,7 @@
             <b-card-text>บทที่2</b-card-text>
         </b-card>
         <table class="table bodycard table-borderless text-center table-hover">
-        <thead>
+        <thead class="childcard">
             <tr>
             <th scope="col">#</th>
             <th scope="col">Word</th>
@@ -18,7 +18,7 @@
             <th scope="row">1</th>
             <td>social networking</td>
             <td>n.</td>
-            <td class="text-left">The use of websites and other internet services to communicate with other people and make friends.</td>
+            <td class="text-left">The use of websites and other internet services to communicate with other <br>people and make friends.</td>
             </tr>
             <tr>
             <th scope="row">2</th>
@@ -54,13 +54,13 @@
             <th scope="row">7</th>
             <td>cyberbullying</td>
             <td>n.</td>
-            <td class="text-left">Someone who uses the internet to harm or frighten another person, especially by sending them unpleasant messages.</td>
+            <td class="text-left">Someone who uses the internet to harm or frighten another person, especially <br>by sending them unpleasant messages.</td>
             </tr>
             <tr>
             <th scope="row">8</th>
             <td>addiction</td>
             <td>n.</td>
-            <td class="text-left">n. The need or strong desire to do or to have something, or a very strong liking for something.</td>
+            <td class="text-left">n. The need or strong desire to do or to have something, or a very strong <br>liking for something.</td>
             </tr>
             <tr>
             <th scope="row">9</th>
@@ -71,14 +71,14 @@
             <th scope="row">10</th>
             <td>copyright</td>
             <td>n.</td>
-            <td class="text-left">the legal right to control the production and selling of a book, play, film, photograph, or piece of music.</td>
+            <td class="text-left">the legal right to control the production and selling of a book, play, film, <br>photograph, or piece of music.</td>
             </tr>
         </tbody>
         </table>
         <br>
             <div>
                 <b-card class="bodycard text-left">
-                    Exercise
+                    <p>Exercise</p> 
                     <p>Fill in the blanks with the most appropriate words.</p>
                     <p>1. Facebook enables us to __________ a lot more with the community.</p>
                     <p>2. Only manufacturers acting responsibly can do that by controlling game __________.</p>
@@ -91,43 +91,64 @@
                     <p>9. Charlie Puth reached the top 10 on the Billboard Hot with “We Don’t Talk Anymore” last month. He is quite sure he will now have plenty of new___________. </p>
                     <p>10. The United States will not allow Iraq to __________ its neighbors.</p>
                 </b-card>
-                </div>
-                <br>
-                <div>
-        <b-card
-            text-variant="white"
-            style="width: 360px;border-radius: 10px;"
-            class="text-center my-2 bodycard">
-            <b-card-text>Object Pronouns</b-card-text>
-        </b-card>
-        <table class="table bodycard table-borderless text-center">
-            <div>
-                <br>
-                <p>What are object pronouns?</p>
-                    <p class="text-left">&ensp;An object pronoun, also called objective pronoun, functions as the object of a verb or preposition, as distinguished from a subject or subjective pronoun, which is the subject of a verb.</p>
-                    <li class="text-left">He begged her to live with him (“her” is the object of the verb “begged” and “him” is the object of the preposition “with”).</li>
-                    <li class="text-left">She told them the truth (“them” is the object of the verb “told”).</li>
-                    <br>
-                    <p class="text-left">&ensp;Object pronouns are used instead of object nouns, usually because we already know what the object is</p>
-                    <li class="text-left">She's my friend. I really enjoy being with her.</li>
-                    <li class="text-left">I like this film. I saw it last week.</li>
-                    <br>
-                <p>Object Pronouns</p>
-                    <p class="text-left">&ensp;Object pronouns in English are the following : me, you, him, her, it, us, them </p>
-                    <p class="text-left">&ensp;Object pronouns come after either a verb (e.g "like") or a preposition (e.g "to").</p>
-                    <li class="text-left">I like you but you don't like me.</li>
-                    <li class="text-left">Do you really hate her?</li>
-                    <li class="text-left">She loves sitting next to him.</li>
-                    <li class="text-left">She always writes e-mails to us.</li>
-                    <br>
             </div>
+                <br>
+        <div>
+        <div>
+        <b-card class="table bodycard table-borderless text-center mt-3" style="text-align: -webkit-center;">
+        <table >
+            <br>
+            <p class='font-big'>Object Pronouns</p>
+                <table class="table bodycard table-borderless text-center mt-3" style="text-align: -webkit-center;">
+                <thead>
+                    <tr>
+                        <p class='font-big childcard'>What are object pronouns?</p>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <p class="text-left">&ensp;An object pronoun, also called objective pronoun, functions as the object of a verb or preposition, as distinguished from a subject or subjective pronoun, which is the subject of a verb.</p>
+                        <li class="text-left">He begged her to live with him (“her” is the object of the verb “begged” and “him” is the object of the preposition “with”).</li>
+                        <li class="text-left">She told them the truth (“them” is the object of the verb “told”).</li>
+                        <br>
+                        <p class="text-left">&ensp;Object pronouns are used instead of object nouns, usually because we already know what the object is</p>
+                        <li class="text-left">She's my friend. I really enjoy being with her.</li>
+                        <li class="text-left">I like this film. I saw it last week.</li>
+                        <br>
+                    </tr>
+                </tbody>
+                </table>
+            <tr>
+                <table class="table bodycard table-borderless text-center mt-3" style="text-align: -webkit-center;">
+                <thead>
+                    <tr>
+                        <p class='font-big childcard'>Object Pronouns</p>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <p class="text-left">&ensp;Object pronouns in English are the following : me, you, him, her, it, us, them </p>
+                        <p class="text-left">&ensp;Object pronouns come after either a verb (e.g "like") or a preposition (e.g "to").</p>
+                        <li class="text-left">I like you but you don't like me.</li>
+                        <li class="text-left">Do you really hate her?</li>
+                        <li class="text-left">She loves sitting next to him.</li>
+                        <li class="text-left">She always writes e-mails to us.</li>
+                        <br>
+                    </tr>
+                </tbody>
+                </table>
+            </tr>
         </table>
+        </b-card>
+        </div>
+
+        
         <table class="table bodycard table-borderless text-center">
             <thead>
             <tr>
                 <th class="text-center font-big" colspan="5" scope="col">Pronouns</th>
             </tr>
-            <tr>
+            <tr class='childcard'>
                 <th scope="col">Subject pronouns</th>
                 <th scope="col">Object pronouns</th>
                 <th scope="col">Possessive adjectives</th>
